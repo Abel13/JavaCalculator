8ccf6221189e499738c0870f79c98111b3ec1178
Abel <abel.o.d@outlook.com>
2022-05-04 23:26:25 -0300

    add method Get Name and Get Symbol

--- src/Service/Operation.java
+++ src/Service/Operation.java
@@ -2,4 +2,8 @@ package Service;
 
 public abstract class Operation {
 	public abstract double Execute(double a, double b);
+
+	public abstract String GetName();
+
+	public abstract String GetSymbol();
 }

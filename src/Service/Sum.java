e62c561710e2512bd44daede25548d47c408203e
Abel <abel.o.d@outlook.com>
2022-05-08 12:58:16 -0300

    Update Sum class with new methods

--- src/Service/Sum.java
+++ src/Service/Sum.java
@@ -1,7 +1,18 @@
 package Service;
 
 public class Sum extends Operation {
+  @Override
   public double Execute(double a, double b) {
     return a + b;
-  }  
+  }
+
+  @Override
+  public String GetName() {
+    return "Soma";
+  }
+
+  @Override
+  public String GetSymbol() {
+    return "+";
+  }
 }

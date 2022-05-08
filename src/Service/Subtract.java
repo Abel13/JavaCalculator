4b43c34e6355c0be523cd63ff67e8397dfad22ee
Abel <abel.o.d@outlook.com>
2022-05-08 12:58:33 -0300

    Update Subtract class with new methods

--- src/Service/Subtract.java
+++ src/Service/Subtract.java
@@ -3,5 +3,15 @@ package Service;
 public class Subtract extends Operation {
   public double Execute(double a, double b) {
     return a - b;
-  }  
+  }
+
+  @Override
+  public String GetName() {
+    return "Subtração";
+  }
+
+  @Override
+  public String GetSymbol() {
+    return "-";
+  }
 }

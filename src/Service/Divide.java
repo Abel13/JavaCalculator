1faa1baa823f23285e790276fc6173d429d012e1
Abel <abel.o.d@outlook.com>
2022-05-08 12:59:29 -0300

    Update Divide class with new methods

--- src/Service/Divide.java
+++ src/Service/Divide.java
@@ -2,9 +2,20 @@ package Service;
 
 public class Divide extends Operation {
   public double Execute(double a, double b) {
-    if(b == 0) {
+    if (b == 0) {
       throw new ArithmeticException("Não é possível dividir por zero");
     }
     return a / b;
   }
+
+  @Override
+  public String GetName() {
+    return "Divisão";
+  }
+
+  @Override
+  public String GetSymbol() {
+    return "÷";
+  }
+  
 }

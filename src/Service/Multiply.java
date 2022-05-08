ed3a01a301e951a6623d41473b8c6025bb69fc57
Abel <abel.o.d@outlook.com>
2022-05-08 12:59:09 -0300

    Update Multiply class with new methods

--- src/Service/Multiply.java
+++ src/Service/Multiply.java
@@ -1,7 +1,18 @@
 package Service;
 
 public class Multiply extends Operation {
+  @Override
   public double Execute(double a, double b) {
     return a * b;
+  }
+
+  @Override
+  public String GetName() {
+    return "Multiplicação";
+  }
+
+  @Override
+  public String GetSymbol() {
+    return "×";
   }
 }
